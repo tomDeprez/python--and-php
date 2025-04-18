552cb53cf76150d16d1f4ec8d43338952caf79bb
Tom DEPREZ <tom_deprez@hotmail.fr>
2025-04-18 15:13:42 +0200

    test

---
+++ index.py
@@ -0,0 +1,29 @@
+tom = 25; salut = "ok"
+
+tom
+
+tom = "toto"
+
+demobool = True
+demobool = False
+
+#          0    1   2   3
+demotbl = [10, 20, 30, "40"]
+
+
+test = demotbl[3] + "3"
+
+test = 3
+
+
+
+
+
+
+
+
+
+#
+notes = ["Bonjour", 18,["Salut", "Super !", [1, 2, 3]], 20, 14, [0, 5]]
+
+print(notes[2][0])
